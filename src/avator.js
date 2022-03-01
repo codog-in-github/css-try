c04787563dd609873332967a80a51fe27e55140d
zjy <zhaojangyu@gmail.com>
2022-03-01 15:07:00 +0800

    完成

--- src/avator.js
+++ src/avator.js
@@ -5,9 +5,12 @@ import './style/avator.less'
 const main = () => {
     const app = document.getElementById('app')
     const avator = document.createElement('div')
+    const heightLight = document.createElement('div')
 
     app.appendChild(avator)
+    avator.appendChild(heightLight)
     avator.classList.add('avator')
+    heightLight.classList.add('h-l')
 }
 
 document.ready(main)
